11f235a9eb215b437ee8307eccfb77d051a50ca9
RafalbCzajka <rafalbczajka@gmail.com>
2025-05-01 14:40:12 +0100

    refactored api to use express routers

--- app.js
+++ app.js
@@ -2,34 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
-const {getEndpoints} = require("./controllers/api.controller");
-const {getAllTopics} = require("./controllers/topics.controller");
-const {getAllUsers} = require("./controllers/users.controller");
-const {getArticleById, getAllArticles, patchArticle} = require("./controllers/articles.controller");
-const {getCommentsByArticleId, postComment, deleteComment} = require("./controllers/comments.controller");
+const apiRouter = require("./routes/api-router");
 const {invalidPathHandler, serverErrorHandler, customErrorHandler, psqlErrorHandler} = require("./controllers/errors.controller");
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get("/api", getEndpoints);
-
-app.get("/api/topics", getAllTopics);
-
-app.get("/api/users", getAllUsers);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.post("/api/articles/:article_id/comments", postComment);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.patch("/api/articles/:article_id", patchArticle);
-
-app.get("/api/articles", getAllArticles);
-
-app.delete("/api/comments/:comment_id", deleteComment);
+app.use("/api", apiRouter);
 
 app.all('*', invalidPathHandler);
 
---
+++ routes/articles-router.js
@@ -0,0 +1,13 @@
+const {getArticleById, getAllArticles, patchArticle} = require("../controllers/articles.controller");
+const {getCommentsByArticleId, postComment} = require("../controllers/comments.controller");
+
+const articlesRouter = require("express").Router();
+
+articlesRouter.get("/", getAllArticles);
+articlesRouter.get("/:article_id", getArticleById);
+articlesRouter.patch("/:article_id", patchArticle);
+
+articlesRouter.get("/:article_id/comments", getCommentsByArticleId);
+articlesRouter.post("/:article_id/comments", postComment);
+
+module.exports = articlesRouter;
