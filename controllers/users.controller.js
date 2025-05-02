ccc6199e127d496d9ccde03fbd48e12aa574dbf6
RafalbCzajka <rafalbczajka@gmail.com>
2025-05-02 14:56:06 +0100

    Implemented endpoint GET /api/users/:username

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,18 @@
-const {fetchAllUsers} = require("../models/users.model");
+const {fetchAllUsers, fetchUserByUsername} = require("../models/users.model");
 
 exports.getAllUsers = (req, res, next) => {
     fetchAllUsers().then((users) => {
         res.status(200).send({users});
+    }).catch(next);
+}
+
+exports.getUserByUsername = (req, res, next) => {
+    const {username} = req.params;
+
+    fetchUserByUsername(username).then((user) => {
+        if (!user) {
+            res.status(404).send({msg: "User not found"});
+        }
+        res.status(200).send({user});
     }).catch(next);
 }
