b0d2651ce6b9f41fc573a388276733d0ac9a8431
RafalbCzajka <rafalbczajka@gmail.com>
2025-06-18 14:55:16 +0100

    Implemented pagination to endpoint GET /api/articles/:article_id/comments

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,14 +1,15 @@
 const db = require("../db/connection");
 const {checkExists} = require("../db/seeds/utils");
 
-exports.fetchCommentsByArticleId = (id) => {
+exports.fetchCommentsByArticleId = (id, limit, offset) => {
     return checkExists('articles', 'article_id', id).then((exists) => {
         if (!exists) {
             return Promise.reject({status: 404, msg: "article_id not found"});
         }
         return db.query(`SELECT comment_id, votes, created_at, author, body, article_id FROM comments
             WHERE article_id = $1
-            ORDER BY created_at DESC`, [id])
+            ORDER BY created_at DESC
+            LIMIT $2 OFFSET $3`, [id, limit, offset])
         .then(({rows}) => {
             return rows;
         })
